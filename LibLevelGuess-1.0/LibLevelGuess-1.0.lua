361757e06bbb2364b01a2a7db2957c77efa748ab
Xilcoy <xilcoy@gmail.com>
2020-10-25 19:50:39 -0500

    Update spell id table

--- LibLevelGuess-1.0/LibLevelGuess-1.0.lua
+++ LibLevelGuess-1.0/LibLevelGuess-1.0.lua
@@ -8,7 +8,7 @@
 ]]
 
 local MAJOR_VERSION = "LibLevelGuess-1.0";
-local MINOR_VERSION = tonumber(("$Revision: 39 $"):match("%d+"));
+local MINOR_VERSION = 50; -- Update Manually
 
 if(not LibStub) then error("LibLevelGuess-1.0 requires LibStub."); end
 
